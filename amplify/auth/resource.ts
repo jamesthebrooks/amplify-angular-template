70be854f18b63d3a1ff237fa76a08a72e2b21752
James Brooks <jamescostonbrooks@gmail.com>
2025-01-17 10:07:22 -0500

    meh

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -4,6 +4,15 @@ import {defineAuth, secret} from '@aws-amplify/backend';
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
+
+/***
+ *
+ *   npx ampx sandbox secret set <secret id>
+ *   npx ampx sandbox secret set <secret value>
+ *   npx ampx sandbox secret list // to test
+ *
+ */
+
 export const auth = defineAuth({
   loginWith: {
     email: true,
