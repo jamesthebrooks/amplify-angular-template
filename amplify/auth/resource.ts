05f8a5a3297c1926b64b5ce23578efdb4cf20d59
James Brooks <jamescostonbrooks@gmail.com>
2025-01-17 08:44:05 -0500

    Added google auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth } from '@aws-amplify/backend';
+import {defineAuth, secret} from '@aws-amplify/backend';
 
 /**
  * Define and configure your auth resource
@@ -7,5 +7,22 @@ import { defineAuth } from '@aws-amplify/backend';
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      google: {
+        clientId: secret('GOOGLE_CLIENT_ID'),
+        clientSecret: secret('GOOGLE_CLIENT_SECRET'),
+        scopes: ['email'],
+      },
+      callbackUrls: [
+        'http://localhost:4200/',
+        'https://www.badowltool.com',
+        'https://dev.dfer2cbwc2thd.amplifyapp.com/',
+      ],
+      logoutUrls: [
+        'http://localhost:4200/',
+        'https://www.badowltool.com',
+        'https://dev.dfer2cbwc2thd.amplifyapp.com/',
+      ],
+    }
   },
 });
